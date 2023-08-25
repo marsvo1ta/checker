3ff54b5f72428fa9dca1d97c4fa485bebb81b811
marsvo1ta <euromobilco@gmail.com>
2023-08-25 15:40:42 +0300

    sending test

--- NPG/tests/test_npg_prod.py
+++ NPG/tests/test_npg_prod.py
@@ -13,24 +13,24 @@ class TestNPG(BaseCase):
         total = response.json()['metadata']['total']
         self.assertGreater(total, 83_000, f'total = {total} statuscode = {response.status_code}')
 
-    def test_get_poland_warehouses(self):
-        params = {'filter[country.code]': 'PL'}
-        response = self.get(f'{NPG_URL_PROD}warehouses', headers=self.auth_manager.prod_admin_auth(), params=params)
-        total = response.json()['metadata']['total']
-        self.assertGreater(total, 60_000, f'total = {total} statuscode = {response.status_code}')
-
-    def test_get_poland_warehouses_by_partner_id(self):
-        params = {'filter[country.code]': 'PL', 'filter[partnerId]': '[176|95]'}
-        response = self.get(f'{NPG_URL_PROD}warehouses', headers=self.auth_manager.prod_admin_auth(), params=params)
-        total = response.json()['metadata']['total']
-        self.assertGreater(total, 20_000, f'total = {total} statuscode = {response.status_code}')
-
-    def test_get_poland_branches_by_partner_id(self):
-        params = {'filter[country.code]': 'PL', 'filter[partnerId]': '[176|95]',
-                  'filter[pointType]': 'branch'}
-        response = self.get(f'{NPG_URL_PROD}warehouses', headers=self.auth_manager.prod_admin_auth(), params=params)
-        total = response.json()['metadata']['total']
-        self.assertGreater(total, 1, f'total = {total} statuscode = {response.status_code}')
+    # def test_get_poland_warehouses(self):
+    #     params = {'filter[country.code]': 'PL'}
+    #     response = self.get(f'{NPG_URL_PROD}warehouses', headers=self.auth_manager.prod_admin_auth(), params=params)
+    #     total = response.json()['metadata']['total']
+    #     self.assertGreater(total, 60_000, f'total = {total} statuscode = {response.status_code}')
+    #
+    # def test_get_poland_warehouses_by_partner_id(self):
+    #     params = {'filter[country.code]': 'PL', 'filter[partnerId]': '[176|95]'}
+    #     response = self.get(f'{NPG_URL_PROD}warehouses', headers=self.auth_manager.prod_admin_auth(), params=params)
+    #     total = response.json()['metadata']['total']
+    #     self.assertGreater(total, 20_000, f'total = {total} statuscode = {response.status_code}')
+    #
+    # def test_get_poland_branches_by_partner_id(self):
+    #     params = {'filter[country.code]': 'PL', 'filter[partnerId]': '[176|95]',
+    #               'filter[pointType]': 'branch'}
+    #     response = self.get(f'{NPG_URL_PROD}warehouses', headers=self.auth_manager.prod_admin_auth(), params=params)
+    #     total = response.json()['metadata']['total']
+    #     self.assertGreater(total, 1, f'total = {total} statuscode = {response.status_code}')
 
     # def test_get_poland_lockers_by_partner_id(self):
     #     params = {'filter[country.code]': 'PL', 'filter[partnerId]': '[176|95]',

cb117694530e21bfe4f6834b602d28cab88e5253
marsvo1ta <euromobilco@gmail.com>
2023-08-28 14:20:19 +0300

    final result

--- telegram_sending.py
+++ telegram_sending.py
@@ -13,9 +13,9 @@ def send_telegram_message(bot_token, chat_id, message):
     return response.json() if response.status_code == 200 else response.status_code
 
 
-# with open('formatted_test_results.txt', 'r') as file:
-#     message = file.read()
+with open('formatted_test_results.txt', 'r') as file:
+    message = file.read()
 bot_token = TELEGRAM_BOT_TOKEN
 chat_id = TELEGRAM_CHAT_ID
 
-send_telegram_message(bot_token, chat_id, 'message2')
+send_telegram_message(bot_token, chat_id, message)

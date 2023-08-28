cab2f584b624f16d6083ff49312310417db87e6e
marsvo1ta <euromobilco@gmail.com>
2023-08-28 14:16:37 +0300

    fix cred

--- data/credentials.py
+++ data/credentials.py
@@ -6,7 +6,7 @@ NPG_URL_PROD = os.environ.get('NPG_URL_PROD')
 NPG_ADMIN_EMAIL = os.environ.get('NPG_ADMIN_EMAIL')
 NPG_ADMIN_PASS = os.environ.get('NPG_ADMIN_PASS')
 
-# TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
-TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_TOKEN')
-TELEGRAM_CHAT_ID = '-926621661'
+TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
+TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
+
 

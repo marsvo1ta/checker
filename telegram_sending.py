0d617198f1b6e8ae08bff35e1d9e84f1cdb6a54b
marsvo1ta <euromobilco@gmail.com>
2023-08-28 14:11:58 +0300

    fix cred

--- telegram_sending.py
+++ telegram_sending.py
@@ -9,12 +9,13 @@ def send_telegram_message(bot_token, chat_id, message):
         'text': message
     }
     response = requests.post(url, json=payload)
-    return response.json()
+    print(response.status_code)
+    return response.json() if response.status_code == 200 else response.status_code
 
 
-with open('formatted_test_results.txt', 'r') as file:
-    message = file.read()
+# with open('formatted_test_results.txt', 'r') as file:
+#     message = file.read()
 bot_token = TELEGRAM_BOT_TOKEN
 chat_id = TELEGRAM_CHAT_ID
 
-send_telegram_message(bot_token, chat_id, message)
+send_telegram_message(bot_token, chat_id, 'message')

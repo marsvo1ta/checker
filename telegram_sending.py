3a85596dba99240179d6bba606bcca37cbbda512
marsvo1ta <84044344+marsvo1ta@users.noreply.github.com>
2023-08-30 00:08:59 +0300

    Update telegram_sending.py

--- telegram_sending.py
+++ telegram_sending.py
@@ -9,15 +9,15 @@ def get_current_time(timezone='Europe/Kiev', format='%d.%m.%Y %H:%M:%S'):
     tz = pytz.timezone(timezone)
     current_time = datetime.now(tz)
     formatted_time = current_time.strftime(format)
-    return f'**{formatted_time}**'
+    return f'*{formatted_time}*'
 
 
-def send_telegram_message(bot_token, chat_id, message):
+def send_telegram_message(bot_token, chat_id, message, parse_mode='None'):
     url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
     payload = {
         'chat_id': chat_id,
         'text': message,
-        
+        'parse_mode': parse_mode
     }
     response = requests.post(url, json=payload)
     print(response.status_code)
@@ -45,5 +45,5 @@ if 'FAIL' in message.split():
     send_telegram_message(bot_token, chat_id, f'{get_current_time()}\n\n{message}')
     send_telegram_sticker(bot_token, chat_id, FAIL)
 else:
-    send_telegram_message(bot_token, chat_id, get_current_time())
+    send_telegram_message(bot_token, chat_id, get_current_time(), 'Markdown')
     send_telegram_sticker(bot_token, chat_id, PASS)

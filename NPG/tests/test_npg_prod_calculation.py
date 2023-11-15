3088a8c15024000b3f7be76f8a777d11290c59ad
marsvo1ta <euromobilco@gmail.com>
2023-11-15 14:11:18 +0200

    gb-ua and ua-pl tests

--- NPG/tests/test_npg_prod_calculation.py
+++ NPG/tests/test_npg_prod_calculation.py
@@ -1,40 +1,56 @@
-import os
-
-
 import requests
 
 from basecase import BaseCase
-from data.credentials import NPG_URL_PROD, NPG_URL_STAGE
+from data.credentials import NPG_URL_PROD
 from auth.authorization import AuthManager
 from json_data.json_serialize import json_serialize
 from json_data.json_file_path import get_json_file_path
 
 
 class TestCalculate(BaseCase):
+
     def setUp(self):
         super().setUp()
         self.auth_manager = AuthManager()
+        self.url = f'{NPG_URL_PROD}international-express-waybills/calculate'
+        self.auth = self.auth_manager.prod_admin_auth()
+
+    def json_data(self, file_name: str):
+        json_data = json_serialize(get_json_file_path(file_name))
+        response = requests.post(self.url, headers=self.auth, json=json_data)
+        return response
 
     def test_CA_UA(self):
-        url = f'{NPG_URL_PROD}international-express-waybills/calculate'
-        auth = self.auth_manager.prod_admin_auth()
-
-        json_data = json_serialize(get_json_file_path('CA_UA_prod'))
-        response = requests.post(url, headers=auth, json=json_data)
-
+        response = self.json_data('CA_UA_prod')
         self.assertEqual(response.status_code, 200)
-
+        print()
         print(response.json()['deliveryPriceCost'])
         print(response.json()['domesticCurrencyCost'])
 
     def test_US_UA(self):
-        url = f'{NPG_URL_PROD}international-express-waybills/calculate'
-        auth = self.auth_manager.prod_admin_auth()
-
-        json_data = json_serialize(get_json_file_path('US_UA_prod'))
-        response = requests.post(url, headers=auth, json=json_data)
-
+        response = self.json_data('US_UA_prod')
         self.assertEqual(response.status_code, 200)
+        print()
+        print(response.json()['deliveryPriceCost'])
+        print(response.json()['domesticCurrencyCost'])
 
+    def test_GB_UA(self):
+        response = self.json_data('GB_UA_prod')
+        self.assertEqual(response.status_code, 200)
+        print()
+        print(response.json()['deliveryPriceCost'])
+        print(response.json()['domesticCurrencyCost'])
+
+    def test_UA_PL_global(self):
+        response = self.json_data('UA_PL_global_delivery_prod')
+        self.assertEqual(response.status_code, 200)
+        print()
+        print(response.json()['deliveryPriceCost'])
+        print(response.json()['domesticCurrencyCost'])
+
+    def test_UA_PL_things(self):
+        response = self.json_data('UA_PL_things_prod')
+        self.assertEqual(response.status_code, 200)
+        print()
         print(response.json()['deliveryPriceCost'])
         print(response.json()['domesticCurrencyCost'])

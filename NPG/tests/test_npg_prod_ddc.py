062b09137fd6912f9fd2811b49d1b150026945ff
marsvo1ta <euromobilco@gmail.com>
2023-12-01 17:59:54 +0200

    ddc

--- NPG/tests/test_npg_prod_ddc.py
+++ NPG/tests/test_npg_prod_ddc.py
@@ -11,12 +11,12 @@ class TestCalculate(BaseCase):
 
     def setUp(self):
         super().setUp()
-        self.url = f'https://npg-ddc-backend-prod.c1.npshopping.com/v1.0/search/cargo?country-code=ua&locale=uk'
+        self.url = 'https://npg-ddc-backend-prod.c1.npshopping.com/v1.0/search/cargo?country-code=ua&locale=uk'
 
     def test_ddc(self):
         keywords_list = ['куртка', 'шорти', 'мʼяч', 'штани']
         for idx, word in enumerate(keywords_list):
-            response = requests.get(f'self.url{word}')
+            response = requests.get(f'{self.url}{word}')
             print(response.json())
             self.assertIsNotNone(response.json()['items'])
 

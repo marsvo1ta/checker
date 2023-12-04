fb00f429ba2890e773f479dfe490635f8478a9ce
marsvo1ta <euromobilco@gmail.com>
2023-12-04 09:26:21 +0200

    ddc

--- NPG/tests/test_npg_prod_ddc.py
+++ NPG/tests/test_npg_prod_ddc.py
@@ -1,22 +1,33 @@
 import requests
-
 from basecase import BaseCase
-from data.credentials import NPG_URL_PROD
-from auth.authorization import AuthManager
-from json_data.json_serialize import json_serialize
-from json_data.json_file_path import get_json_file_path
+from data.credentials import NPG_DDC_PROD
 
 
 class TestCalculate(BaseCase):
 
     def setUp(self):
         super().setUp()
-        self.url = 'https://npg-ddc-backend-prod.c1.npshopping.com/v1.0/search/cargo?country-code=ua&locale=uk'
+        self.url = f'{NPG_DDC_PROD}search/cargo?country-code=ua&locale=uk&keyword='
 
-    def test_ddc(self):
+    def test_ddc_ua(self):
         keywords_list = ['куртка', 'шорти', 'мʼяч', 'штани']
         for idx, word in enumerate(keywords_list):
-            response = requests.get(f'{self.url}{word}')
-            print(response.json())
-            # self.assertIsNotNone(response.json()['items'])
+            request_url = f'{self.url}{word}'
+            response = requests.get(request_url)
+            self.assertIsNotNone(response.json()['items'], word)
 
+    def test_ddc_us(self):
+        keywords_list = ['coat', 'shorts', 'ball', 'pants']
+        self.url = self.url.replace('locale=uk', 'locale=en')
+        for idx, word in enumerate(keywords_list):
+            request_url = f'{self.url}{word}'
+            response = requests.get(request_url)
+            self.assertIsNotNone(response.json()['items'], word)
+
+    def test_ddc_pl(self):
+        keywords_list = ['kurtka', 'spodenki', 'piłka', 'spodnie']
+        self.url = self.url.replace('locale=uk', 'locale=pl')
+        for idx, word in enumerate(keywords_list):
+            request_url = f'{self.url}{word}'
+            response = requests.get(request_url)
+            self.assertIsNotNone(response.json()['items'], word)

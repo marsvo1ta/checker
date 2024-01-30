77711e11a66e0e3d859a0bca5003cf61fec06955
marsvo1ta <euromobilco@gmail.com>
2024-01-30 11:05:41 +0200

    add country_codes

---
+++ data/country_codes/country_codes.py
@@ -0,0 +1,30 @@
+CODES = ["ua", "pl", "ro", "md", "cz", "us", "de", "lt", "ca", "gb",
+              "cn", "es", "fr", "il", "lv", "nl", "it", "ie", "ch", "tr",
+              "ge", "sk", "ee", "at", "cy", "au", "pt", "bg", "hu", "be",
+              "dk", "se", "in", "no", "az", "fi", "hr", "jp", "si", "mx",
+              "sg", "br", "kr", "rs", "ma", "th", "lu", "za", "pk", "nz",
+              "bd", "my", "ph", "mt", "ar", "is", "pr", "ad"]
+
+KEYWORDS_EN = ["Kyiv", "Warsaw", "Bucuresti", "Chisinau", "Praha",
+                    "Washington", "Berlin", "Vilnius", "Ottawa", "London",
+                    "Beijing", "Madrid", "Paris", "Jerusalem", "Riga", "Amsterdam",
+                    "Rome", "Dublin", "Bern", "Ankara", "Tbilisi", "Bratislava",
+                    "Tallinn", "Vienna", "Nicosia", "Canberra", "Lisbon", "Sofia",
+                    "Budapest", "Brussels", "Copenhagen", "Stockholm", "New Delhi",
+                    "Oslo", "Baku", "Helsinki", "Zagreb", "Tokyo", "Ljubljana",
+                    "Mexico City", "Singapore", "Brasília", "Seoul", "Belgrade",
+                    "Rabat", "Bangkok", "Luxembourg", "Pretoria", "Islamabad",
+                    "Wellington", "Dhaka", "Kuala Lumpur", "Manila", "Valletta",
+                    "Buenos Aires", "Reykjavik", "San Juan", "Andorra la Vella"]
+
+KEYWORDS_UA = ["Київ", "Варшава", "Бухарест", "Кишинів", "Прага",
+                    "Вашингтон", "Берлін", "Вільнюс",
+                    "Оттава", "Лондон", "Пекін", "Мадрид", "Париж", "Єрусалим",
+                    "Рига", "Амстердам", "Рим", "Дублін", "Берн", "Анкара", "Тбілісі",
+                    "Братислава", "Таллінн", "Відень", "Нікосія", "Канберра",
+                    "Лісабон", "Софія", "Будапешт", "Брюссель", "Копенгаген",
+                    "Стокгольм", "Нью-Делі", "Осло", "Баку", "Гельсінкі",
+                    "Загреб", "Токіо", "Любляна", "Мехіко", "Сінгапур", "Бразиліа",
+                    "Сеул", "Белград", "Рабат", "Бангкок", "Люксембург", "Преторія",
+                    "Ісламабад", "Веллінгтон", "Дакка", "Куала-Лумпур", "Маніла",
+                    "Валетта", "Буенос-Айрес", "Рейкʼявік", "Сан-Хуан", "Андорра-ла-Велья"]

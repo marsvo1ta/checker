b2a9babb8975f232703da0a2caff76f88b4c1247
marsvo1ta <euromobilco@gmail.com>
2024-02-02 17:44:08 +0200

    test

--- auth/partners_api_auth.py
+++ auth/partners_api_auth.py
@@ -1,5 +1,5 @@
 import requests
-from data.credentials import PARTNERS_PASS, PARTNERS_LOGIN, NPS_URL_STAGE, NPS_EURO_MAIL, NPS_PASS_PROD
+from data.credentials import *
 
 class PartnersAuth:
     def __init__(self):
@@ -12,6 +12,7 @@ class PartnersAuth:
         self.user_auth_url = f'{self.url}api/user/login/password'
         self.back_token = None
         self.user_token = None
+
     def get_token_back(self):
         basic_auth = (self.basic_login, self.basic_password)
         body = {"login": "test_test", "password": "Password1"}
@@ -27,11 +28,22 @@ class PartnersAuth:
 
         return self.user_token
 
+    def login(self, environment):
+        body = {
+            'email': NPG_ADMIN_EMAIL,
+            'password': NPG_ADMIN_PASS
+        }
+        url = f'{NPG_URL_STAGE if environment == "stage" else NPG_URL_PROD}login/admin'
+        response = requests.post(url, json=body)
+        if response.status_code == 200:
+            return response.json()['token']
+        return None
 
 t = PartnersAuth()
 t.get_token_back()
 t.get_token_user()
 
 def test():
-    print(t.back_token)  # виводимо токен для back_auth
-    print(t.user_token)  # виводимо токен для user_auth
+    print(t.login)
+    # print(t.back_token)  # виводимо токен для back_auth
+    # print(t.user_token)  # виводимо токен для user_auth

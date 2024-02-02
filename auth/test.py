9079950fe39769e7592c1e72b372de47b449ae12
marsvo1ta <euromobilco@gmail.com>
2024-02-02 12:44:44 +0200

    test

--- auth/test.py
+++ auth/test.py
@@ -1,12 +1,15 @@
 import requests
 
+basic_login = str('nova_post')
+basic_password = str('Hg71FjI95hB3aS0L')
+url = 'https://npshopping-stag.c1.npshopping.com/api/partners/login'
 
-def test_get_token_back(self):
-    basic_auth = (self.basic_login, self.basic_password)
+
+def test_get_token_back():
+    basic_auth = (basic_login, basic_password)
     body = {"login": "test_test",
             "password": "Password1"}
-
-    response = requests.post(self.back_auth_url,
+    response = requests.post(url,
                              auth=basic_auth,
                              json=body)
     print(response.text)

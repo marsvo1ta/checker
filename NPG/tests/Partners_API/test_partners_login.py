04d7179bedba7fd1ad74b27287d233c30a6088e9
marsvo1ta <euromobilco@gmail.com>
2024-02-05 11:23:01 +0200

    test

--- NPG/tests/Partners_API/test_partners_login.py
+++ NPG/tests/Partners_API/test_partners_login.py
@@ -6,8 +6,10 @@ class TestLogin(BaseCase):
     def setUp(self):
         super().setUp()
         self.auth_manager = PartnersAuth()
-
+        self.auth_manager.get_token_back()
+        self.auth_manager.get_token_user()
 
     def test_auth(self):
-        self.auth_manager.get_token_back()
+
         self.assertIsNotNone(self.auth_manager.back_token)
+        self.assertIsNotNone(self.auth_manager.user_token)

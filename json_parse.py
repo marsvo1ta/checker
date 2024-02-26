4673fbd8d448661d5fb31d5d687578a46404f1d0
marsvo1ta <euromobilco@gmail.com>
2024-02-26 14:10:30 +0200

    add counter

--- json_parse.py
+++ json_parse.py
@@ -4,10 +4,10 @@ with (open('report.json', 'r') as f,
       open('formatted_test_results.txt', 'w') as output_file):
     results = json.load(f)
     tests = results['report']['tests']
-    for test in tests:
+    for idx, test in enumerate(tests, start=1):
         status = test['outcome'].upper()
         test_name = ' '.join(test['name'].split('::')[-2:])
-        output = f'{status} {test_name}\n\n'
+        output = f'{idx}.{status} {test_name}\n\n'
         output_file.write(output)
 
 
